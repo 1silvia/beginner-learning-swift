758d0b3792a36ebeb1025642d1e1a16b01cc3870
Silvia Istudor <istudor.silvia@gmail.com>
2019-05-29 22:25:03 +0200

    create variables of different types

---
+++ Variables/Variables.playground/Contents.swift
@@ -0,0 +1,47 @@
+//Define a variable that takes a value of type String. Change the value in the same variable
+
+var str: String = "Hello, playground1"
+
+str = "Goodbye!"
+str = "Hello again!"
+
+//Define a variable that takes a value of type Int (Integer). Change the value in the same variable
+
+var age: Int = 38
+age = 39
+
+var population = 8_000_000
+population = 8_000_001
+
+//Define a variable that takes a value of type Double. Change the value in the same variable
+
+var double: Double = 3.45
+double = 3.44
+
+//Define a variable that takes a value of type Float. Change the value in the same variable
+
+var price: Float = 5.46
+price = 4.46
+
+//Define a variable that takes a value of type Bool (Boolean). Change the value in the same variable
+
+var awesome: Bool = true
+awesome = false
+
+//Insert a variable in a string
+
+var score = "Your score is"
+var valueScore = 87
+
+var result = "\(score) \(valueScore)"
+
+//Define a variable that takes a value of type String that goes into two separate rows. Change the value in the same variable
+
+var poem = """
+this goes
+over multiple lines
+"""
+poem = """
+Main Title
+Subtitle
+"""
