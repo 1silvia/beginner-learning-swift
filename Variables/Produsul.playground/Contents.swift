4a882107186a1943c08dd75edabd35f6647164ff
Silvia Istudor <istudor.silvia@gmail.com>
2020-03-14 15:44:30 +0100

    Using .map and .reduce functions to calculate or change items in array

--- Variables/Produsul.playground/Contents.swift
+++ Variables/Produsul.playground/Contents.swift
@@ -4,16 +4,30 @@ func multiply(number1: Float, number2: Float) -> Float {
 
 multiply(number1: 3.5, number2: 5.6)
 
-//given an array, right a function that calculates the sum of all the elements
+//given an array, write a function that calculates the sum of all the elements
 
 func getSum (input: [Int]) -> Int {
     var sum = 0
     for index in 0...input.count - 1 {
         sum += input[index]
     }
+    
     return sum
 }
 
-getSum(input: [1, 3, 4, 6])
+func getSumAdvanced (input: [Int]) -> Int {
+    return input.reduce(0) { previousResult, element in
+        previousResult + element * 10
+    }
+}
 
+func change(input: [Int]) -> [Int] {
+    return input.map { element in
+        element * 10
+    }
+}
+
+getSum(input: [1, 3, 4, 6])
+getSumAdvanced(input: [1, 3, 4, 6])
+change(input: [1, 3, 4, 6])
 
