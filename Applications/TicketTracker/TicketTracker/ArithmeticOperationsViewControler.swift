30946cf30270ef2119683c980c0b4e21127d6e70
Silvia Istudor <istudor.silvia@gmail.com>
2020-03-15 19:53:44 +0100

    Say Hi button

---
+++ Applications/TicketTracker/TicketTracker/ArithmeticOperationsViewControler.swift
@@ -0,0 +1,46 @@
+//
+//  ArithmeticOperationsViewControler.swift
+//  TicketTracker
+//
+//  Created by Silvia Istudor on 2020-03-15.
+//  Copyright © 2020 Silvia Istudor. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ArithmeticOperationsViewControler: UIViewController {
+
+//           guard let number1Text = number1Field.text,
+//               let number1 = Int(number1Text) else {
+//                   print("number1 input or not an integer")
+//                   return
+//           }
+//
+//           guard let number2Text = number2Field.text,
+//               let number2 = Int(number2Text) else {
+//                   print("number2 input or not an integer")
+//                   return
+//           }
+    
+    
+   
+    @IBAction func helloTapped(_ sender: Any) {
+        say(greeting: "Hello", name: nameTextField.text!)
+    }
+    
+    @IBAction func saluteTapped(_ sender: Any) {
+        say(greeting: "Hi", name: nameTextField.text!)
+    }
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var saluteLabel: UILabel!
+    
+    func say(greeting: String, name: String) {
+        
+        saluteLabel.text = greeting + " " + name
+    }
+    
+}
+
+
